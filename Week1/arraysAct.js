35b70d274608ab82c90d4e5450627ab9f4ed1788
Edward Valentine <eddieval2022@gmail.com>
2022-05-16 19:01:33 +0100

    Arrays activities

---
+++ Week1/arraysAct.js
@@ -0,0 +1,20 @@
+// //Activity 1
+
+
+let favouriteSongs = ["mobb deep - Shook ones",
+"coops - laws of nature",
+"jaykae - toothache",];
+console.log(favouriteSongs);
+favouriteSongs.push ("pendulum - tarantula","AC-DC - Back in black");
+console.log (favouriteSongs);
+favouriteSongs.pop();
+console.log (favouriteSongs);
+
+//Activity 2
+
+let favouriteDrinks = ["Latte", 
+"Orange",
+"Ribena",];
+console.log(favouriteDrinks);
+favouriteDrinks.splice (0,0,"Vimto");
+console.log (favouriteDrinks);
